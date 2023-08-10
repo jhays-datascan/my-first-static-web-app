37ed042caed6af44d41277655aa6238e582adec3
Justin Hays <jhays@datascan.com>
2023-08-10 11:47:38 -0500

    added listener for parent

--- src/App.js
+++ src/App.js
@@ -2,7 +2,20 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value}</div>;
+  let parentMessage = { message: 'default' };
+
+  window.addEventListener('message', (event) => {
+    if(event.origin === 'https://localhost:44333/Shortcut/Admin' || event.origin === 'https://dart.datascan.com/Shortcut/Admin') {
+      parentMessage = event.data;
+    }
+  });
+
+  return (
+    <div>
+      <h1>Hello {value}</h1>
+      <div>Message from parent: { parentMessage.message }</div>
+    </div>
+  );
 }
 
 export default App;
